b21fc9c12cbdc1801275ff5311b659eac30ef5fb
fran.oses <fra.oses@duocuc.cl>
2025-10-17 20:04:03 -0300

    borre layout no funciono

--- d-proyect/src/main.jsx
+++ d-proyect/src/main.jsx
@@ -4,7 +4,6 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import "bootstrap-icons/font/bootstrap-icons.css"
-import Layout from './Components/Layout.jsx';
 import Home from './Pages/Home.jsx'
 import Productos from './Pages/Productos.jsx'
 import Login from './Pages/Login.jsx'
