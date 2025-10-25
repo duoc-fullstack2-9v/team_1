bdb5b99f972d3bf715a0810dc57bad2ce5fc9f86
fran.oses <fra.oses@duocuc.cl>
2025-10-25 00:31:22 -0300

    le agregue las paginas a los iconos de redes para q mientras lleven a algo

--- d-proyect/src/Components/Footer.jsx
+++ d-proyect/src/Components/Footer.jsx
@@ -2,7 +2,8 @@ export default function Footer() {
   return (
     <footer className="footer bg-dark text-white py-4">
       <div className="container">
-        <div className="row">
+        <div className="row align-items-center">
+          {/* Columna izquierda */}
           <div className="col-md-4">
             <h5>
               <i className="bi bi-snow"></i> Helados <span className="text-dooc">Dooc</span>
@@ -10,29 +11,65 @@ export default function Footer() {
             <p>Dulces momentos en Iquique desde 2015</p>
           </div>
 
-          <div className="col-md-4">
+          {/* Columna central: Redes */}
+          <div className="col-md-4 text-center">
             <h5>Síguenos</h5>
-            <div className="d-flex gap-3">
-              <a href="#" className="text-white"><i className="bi bi-facebook fs-4"></i></a>
-              <a href="#" className="text-white"><i className="bi bi-instagram fs-4"></i></a>
-              <a href="#" className="text-white"><i className="bi bi-tiktok fs-4"></i></a>
+            <div className="d-flex justify-content-center align-items-center gap-3 mt-2">
+              {/* Facebook */}
+              <a
+                href="https://www.facebook.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white"
+              >
+                <i className="bi bi-facebook fs-4"></i>
+              </a>
+
+              {/* Instagram */}
+              <a
+                href="https://www.instagram.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white"
+              >
+                <i className="bi bi-instagram fs-4"></i>
+              </a>
+
+              {/* TikTok */}
+              <a
+                href="https://www.tiktok.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white"
+              >
+                <i className="bi bi-tiktok fs-4"></i>
+              </a>
             </div>
           </div>
 
+          {/* Columna derecha: Newsletter */}
           <div className="col-md-4">
             <h5>Newsletter</h5>
             <div className="input-group">
-              <input type="email" className="form-control" placeholder="Tu email" />
-              <button className="btn btn-dooc" type="button">Enviar</button>
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Tu email"
+              />
+              <button className="btn btn-dooc" type="button">
+                Enviar
+              </button>
             </div>
           </div>
         </div>
 
-        <hr />
+        <hr className="mt-4" />
         <div className="text-center">
-          <p className="mb-0">&copy; 2025 Helados Dooc Iquique - Todos los derechos reservados</p>
+          <p className="mb-0">
+            &copy; 2025 Helados Dooc Iquique - Todos los derechos reservados
+          </p>
         </div>
       </div>
     </footer>
-  )
+  );
 }
